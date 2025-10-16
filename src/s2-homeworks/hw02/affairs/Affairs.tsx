22b43880ffadb6efa4c477c3e5a43ddd39c9b12b
Rommill <rommill19@gmail.com>
2025-10-16 22:00:23 +0300

    Homework #2 complete

---
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Affair from "./affair/Affair";
+import { AffairType, FilterType } from "../HW2";
+import s from "./Affairs.module.css";
+
+type AffairsPropsType = {
+  data: Array<AffairType>; // need to fix any
+  setFilter: (filter: FilterType) => void;
+  deleteAffairCallback: (_id: number) => void;
+  filter: FilterType;
+};
+
+function Affairs({
+  data,
+  setFilter,
+  deleteAffairCallback,
+  filter,
+}: AffairsPropsType) {
+  const setAll = () => setFilter("all");
+  const setHigh = () => setFilter("high");
+  const setMiddle = () => setFilter("middle");
+  const setLow = () => setFilter("low");
+
+  const cnAll = `${s.button} ${s.all}${filter === "all" ? " " + s.active : ""}`;
+  const cnHigh = `${s.button} ${s.high}${
+    filter === "high" ? " " + s.active : ""
+  }`;
+  const cnMiddle = `${s.button} ${s.middle}${
+    filter === "middle" ? " " + s.active : ""
+  }`;
+  const cnLow = `${s.button} ${s.low}${filter === "low" ? " " + s.active : ""}`;
+
+  const mappedAffairs = data.map((a) => (
+    <Affair
+      key={a._id}
+      affair={a}
+      deleteAffairCallback={deleteAffairCallback}
+    />
+  ));
+
+  return (
+    <div>
+      <div className={s.buttonContainer}>
+        <button id="hw2-button-all" onClick={setAll} className={cnAll}>
+          All
+        </button>
+        <button id="hw2-button-high" onClick={setHigh} className={cnHigh}>
+          High
+        </button>
+        <button id="hw2-button-middle" onClick={setMiddle} className={cnMiddle}>
+          Middle
+        </button>
+        <button id="hw2-button-low" onClick={setLow} className={cnLow}>
+          Low
+        </button>
+      </div>
+
+      <div className={s.affairs}>{mappedAffairs}</div>
+    </div>
+  );
+}
+
+export default Affairs;
---
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { AffairType } from "../../HW2";
+import s from "./Affair.module.css";
+import s2 from "../Affairs.module.css";
+
+type AffairPropsType = {
+  // key не нужно типизировать
+  affair: AffairType;
+  deleteAffairCallback: (_id: number) => void; // need to fix any
+};
+
+function Affair({ affair, deleteAffairCallback }: AffairPropsType) {
+  const deleteCallback = () => {
+    deleteAffairCallback(affair._id);
+    // need to fix
+  };
+
+  const nameClass = `${s.name} ${s2[affair.priority]}`;
+  const buttonClass = `${s.closeButton} ${s2[affair.priority]}`;
+  const affairClass = `${s.affair} ${s2[affair.priority]}`;
+
+  return (
+    <div id={"hw2-affair-" + affair._id} className={affairClass}>
+      <div id={"hw2-name-" + affair._id} className={nameClass}>
+        {affair.name}
+        {/*создаёт студент*/}
+
+        {/**/}
+      </div>
+      <div id={"hw2-priority-" + affair._id} hidden>
+        {affair.priority}
+      </div>
+
+      <button
+        id={"hw2-button-delete-" + affair._id}
+        className={buttonClass}
+        onClick={deleteCallback}
+        // need to fix
+      >
+        {/*текст кнопки могут изменить студенты*/}X{/**/}
+      </button>
+    </div>
+  );
+}
+
+export default Affair;
