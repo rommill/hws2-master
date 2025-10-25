084252e7603b05626311b207d628ac3626baa3ce
Rommill <rommill19@gmail.com>
2025-10-25 16:07:24 +0300

    Добавил Cypress и подавление ошибки [open]:not(:modal)

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,4 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message.includes("[open]:not(:modal)")) {
+    return false;
+  }
+});
